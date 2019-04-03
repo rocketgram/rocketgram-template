83cdb362cbd0f0939053363ffc01772c5294aeb7
vd <vd@>
2019-04-03 07:59:10 +0300

    WEBHOOK_PATH now optional.

--- app/engine.py
+++ app/engine.py
@@ -8,7 +8,7 @@ logger = logging.getLogger('minibots.engine')
 
 
 def main():
-    mode = os.environ.get('MODE', 'updates')
+    mode = os.environ.get('MODE', 'updates').lower().strip()
 
     logging.basicConfig(format='%(asctime)s - %(levelname)-5s - %(name)-25s: %(message)s')
     logging.basicConfig(level=logging.ERROR)
@@ -18,22 +18,22 @@ def main():
     logging.getLogger('rocketgram.raw.in').setLevel(logging.INFO)
     logging.getLogger('rocketgram.raw.out').setLevel(logging.INFO)
 
-    logger.info('Starting bot''s template %s...', )
+    logger.info('Starting bot''s template in %s...', mode)
 
-    bot = mybot.get_bot(os.environ['TOKEN'])
+    bot = mybot.get_bot(os.environ['TOKEN'].strip())
 
     if mode == 'updates':
-        rocketgram.run_updates(bot, drop_updates=bool(int(os.environ.get('DROP_UPDATES', 0))))
+        rocketgram.run_updates(bot, drop_updates=bool(int(os.environ.get('DROP_UPDATES', 0).strip())))
     elif mode in ('webhook', 'heroku'):
         if mode == 'heroku':
             port = os.environ['PORT']
         else:
-            port = os.environ.get('WEBHOOK_PORT', 8080)
+            port = os.environ.get('WEBHOOK_PORT', 8080).strip()
         rocketgram.run_webhook(bot,
-                               os.environ['WEBHOOK_URL'],
-                               os.environ.get('WEBHOOK_PATH', '/'),
+                               os.environ['WEBHOOK_URL'].strip(),
+                               os.environ.get('WEBHOOK_PATH', '/').strip(),
                                port=int(port),
-                               drop_updates=bool(int(os.environ.get('DROP_UPDATES', 0))))
+                               drop_updates=bool(int(os.environ.get('DROP_UPDATES', 0).strip())))
     else:
         raise TypeError('MODE must be `updates` or `webhook` or `heroku`!')
 
