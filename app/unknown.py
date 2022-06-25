26f04b1135ebc1a12e8f98387e874dbd89718aa3
vd <vd@>
2022-06-26 00:31:54 +0300

    Typo.

--- app/unknown.py
+++ app/unknown.py
@@ -8,12 +8,14 @@ from rocketgram import context
 @priority(2048)
 def unknown():
     """\
-    This is about how to use priority.
+    This code shows how to use the priority decorator.
 
-    This handler caches all messages to bot, but since we set priority
-    to 2048 handler will be called if no other handlers was do.
+    This handler will catch all messages to the bot, but since we set the priority
+    to 2048 the handler will be called only if no other handlers was called.
 
-    Default priority in Dispatcher is 1024, so for
-    set the order of handlers you can use @priority decorator."""
+    The default priority in the Dispatcher is 1024.
+
+    You can use the @priority decorator to change it.
+    """
 
     SendMessage(context.user.id, "🔹 I don't known what to do. May be /help?").webhook()
